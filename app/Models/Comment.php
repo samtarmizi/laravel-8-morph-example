0f8ca0f9d06ddbe16d1db5039ffcc7f51618c39c
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2021-01-23 00:29:09 +0800

    model: setup model relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable')->whereNull('parent_id');
+    }
 }
