26a3ee4652023c2cf0a93761a51223739a9fe736
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2021-01-23 00:27:43 +0800

    db: alter posts & comments column

--- database/migrations/2021_01_22_162504_create_posts_table.php
+++ database/migrations/2021_01_22_162504_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->string('slug')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2021_01_22_162544_create_comments_table.php
+++ database/migrations/2021_01_22_162544_create_comments_table.php
@@ -15,6 +15,11 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('comment');
+            $table->integer('user_id')->unsigned();
+            $table->integer('parent_id')->unsigned();
+            $table->integer('commentable_id')->unsigned();
+            $table->string('commentable_type');
             $table->timestamps();
         });
     }
